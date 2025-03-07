89fb49a95dd26a9ba96fa19bd113068e86cf4dd2
Matteus <matteussantos30@gmail.com>
2025-03-07 12:43:44 -0300

    feat: basic dahboard page created

---
+++ src/pages/Dashboard/dashboard.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import {
+    Grommet, Box, Heading, Form, FormField, TextInput, Button, DateInput
+} from 'grommet';
+import { grommet } from 'grommet/themes';
+import { SidebarTip } from '../../components/Sidebar/sidebar';
+
+const Dashboard = () => {
+    const [value, setValue] = useState({});
+
+    return (
+        <Grommet theme={grommet} full>
+            <Box direction="row" fill>
+                <SidebarTip />
+                <Box pad="medium" width="large">
+                    <Heading level="2" color="#3c6aaf">Análise de Materiais</Heading>
+                    
+                </Box>
+            </Box>
+        </Grommet>
+    );
+};
+
+export default Dashboard;
