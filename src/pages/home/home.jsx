e2a12b6978187f2977ee94bd01e3bc278e7358df
Nexusf1re <matteussantos30@hotmail.com>
2025-03-08 14:37:26 -0300

    Refactor: Columns adjusted for the new DB structure

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,77 +1,93 @@
 import React, { useEffect, useState } from 'react';
 import {
-    Grommet, Box, Heading, Table, TableBody, TableCell, TableHeader,
-    TableRow, Card, CardBody, CardHeader
+Grommet, Box, Heading, Table, TableBody, TableCell, TableHeader,
+TableRow, Card, CardBody, CardHeader
 } from 'grommet';
 import { grommet } from 'grommet/themes';
 import { SidebarTip as Sidebar } from '../../components/Sidebar/sidebar';
 
 const Home = () => {
-    const [items, setItems] = useState([]);
+const [items, setItems] = useState([]);
 
-    useEffect(() => {
-        fetch('http://localhost:3000/api/items')
-            .then((res) => res.json())
-            .then((data) => setItems(data))
-            .catch((err) => console.error(err));
-    }, []);
+useEffect(() => {
+fetch('http://localhost:3000/api/items')
+.then((res) => res.json())
+.then((data) => setItems(data))
+.catch((err) => console.error(err));
+}, []);
 
-    return (
-        <Grommet theme={grommet} full>
-            <Box direction="row" fill>
-                <Sidebar />
-                <Box pad="medium" background="light-2" fill>
-                    <Heading level="2" margin="-17px 0px 0px 4px" color="#3c6aaf">
-                        Dados
-                    </Heading>
-                    <Card background="white" margin={{ top: 'small' }} elevation="small" round="small" fill>
-                        <CardHeader pad="medium" background="#3c6aaf">
-                            <Heading level="3" margin="none" color="white">
-                                Stocks | Fresenius Kabi - PU ANÁPOLIS
-                            </Heading>
-                        </CardHeader>
-                        <CardBody pad="medium" fill>
-                            <Table>
-                                <TableHeader>
-                                    <TableRow>
-                                        <TableCell scope="col" border="bottom" background="light-1" pad="medium">
-                                            <strong>RC</strong>
-                                        </TableCell>
-                                        <TableCell scope="col" border="bottom" background="light-1" pad="medium">
-                                            <strong>Material</strong>
-                                        </TableCell>
-                                        <TableCell scope="col" border="bottom" background="light-1" pad="medium">
-                                            <strong>Marca</strong>
-                                        </TableCell>
-                                        <TableCell scope="col" border="bottom" background="light-1" pad="medium">
-                                            <strong>Un</strong>
-                                        </TableCell>
-                                        <TableCell scope="col" border="bottom" background="light-1" pad="medium">
-                                            <strong>Valor</strong>
-                                        </TableCell>
-                                        <TableCell scope="col" border="bottom" background="light-1" pad="medium">
-                                            <strong>Valor NF</strong>
-                                        </TableCell>
-                                    </TableRow>
-                                </TableHeader>
-                                <TableBody>
-                                    {items.map((item) => (
-                                        <TableRow key={item.Id} hover>
-                                            <TableCell pad="medium">{item.RC}</TableCell>
-                                            <TableCell pad="medium">{item.Material}</TableCell>
-                                            <TableCell pad="medium">{item.Marca}</TableCell>
-                                            <TableCell pad="medium">{item.Un}</TableCell>
-                                            <TableCell pad="medium">{item.Valor}</TableCell>
-                                        </TableRow>
-                                    ))}
-                                </TableBody>
-                            </Table>
-                        </CardBody>
-                    </Card>
-                </Box>
-            </Box>
-        </Grommet>
-    );
+return (
+<Grommet theme={grommet} full>
+<Box direction="row" fill>
+<Sidebar />
+<Box pad="medium" background="light-2" fill>
+<Heading level="2" margin="-17px 0px 0px 4px" color="#3c6aaf">
+Dados
+</Heading>
+<Card background="white" margin={{ top: 'small' }} elevation="small" round="small" fill>
+<CardHeader pad="medium" background="#3c6aaf">
+<Heading level="3" margin="none" color="white">
+    Stocks | Fresenius Kabi - PU ANÁPOLIS
+</Heading>
+</CardHeader>
+<CardBody pad="medium" fill>
+<Table>
+    <TableHeader>
+        <TableRow>
+            <TableCell scope="col" border="bottom" background="light-1" p  ad="medium">
+                <strong>RC</strong>
+            </TableCell>
+
+            <TableCell scope="col" border="bottom" background="light-1" pad="medium">
+                <strong>Material</strong>
+            </TableCell>
+
+            <TableCell scope="col" border="bottom" background="light-1" pad="medium">
+                <strong>Qtd</strong>
+            </TableCell>
+
+            <TableCell scope="col" border="bottom" background="light-1" pad="medium">
+                <strong>Valor</strong>
+            </TableCell>
+
+            <TableCell scope="col" border="bottom" background="light-1" pad="medium">
+                <strong>Valor NF</strong>
+            </TableCell>
+            
+            <TableCell scope="col" border="bottom" background="light-1" pad="medium">
+                <strong>Un</strong>
+            </TableCell>
+
+            <TableCell scope="col" border="bottom" background="light-1" pad="medium">
+                <strong>Marca</strong>
+            </TableCell>
+
+            <TableCell scope="col" border="bottom" background="light-1" pad="medium">
+                <strong>Recebimento</strong>
+            </TableCell>
+        </TableRow>
+    </TableHeader>
+    <TableBody>
+        {items.map((item) => (
+            <TableRow key={item.Id} hover>
+                <TableCell pad="medium">{item.RC}</TableCell>
+                <TableCell pad="medium">{item.Material}</TableCell>
+                <TableCell pad="medium">{item.Qtd}</TableCell>
+                <TableCell pad="medium">{item.Valor}</TableCell>
+                <TableCell pad="medium">{item.ValorNF}</TableCell>
+                <TableCell pad="medium">{item.Un}</TableCell>
+                <TableCell pad="medium">{item.Marca}</TableCell>
+                <TableCell pad="medium">{item.Recebimento}</TableCell>
+            </TableRow>
+        ))}
+    </TableBody>
+</Table>
+</CardBody>
+</Card>
+</Box>
+</Box>
+</Grommet>
+);
 };
 
 export default Home;
