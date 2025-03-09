d0d25e1839fcf081a2e2cecedbd359def03f201f
Nexusf1re <matteussantos30@hotmail.com>
2025-03-09 18:04:01 -0300

    Fix: update component imports, add Home, FormField, and Dashboard pages, and adjust routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import React from 'react';
-import Home from './pages/TempHome/Home';
-import Form from './pages/TempFormField/FormField';
-import Dashboard from './pages/TempDashboard/Dashboard';
+import Home from './pages/Home/Home';
+import Form from './pages/FormField/FormField';
+import Dashboard from './pages/Dashboard/Dashboard';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
           <Routes>
-            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Home />} />
              <Route path="/form" element={<Form />} />
              <Route path="/dashboard" element={<Dashboard />} />
           </Routes>
