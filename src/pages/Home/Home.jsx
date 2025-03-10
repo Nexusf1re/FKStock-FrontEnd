61f4435fa1126dc9bd16d2a1aff803724692b0d3
Nexusf1re <matteussantos30@hotmail.com>
2025-03-09 22:12:15 -0300

    Fix: update API URL in Home component to use environment variable

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
 const [items, setItems] = useState([]);
 
 useEffect(() => {
-fetch('http://localhost:3000/api/items')
+fetch(`${process.env.REACT_APP_API_URL}/api/items`)
 .then((res) => res.json())
 .then((data) => setItems(data))
 .catch((err) => console.error(err));
