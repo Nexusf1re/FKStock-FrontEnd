8987af436fc11f2e1704b3cf77ab193ad87c7461
Matteus <matteussantos30@gmail.com>
2025-03-31 12:09:52 -0300

    Feat: replace getUser component with Username component in Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import { grommet } from 'grommet/themes';
 import { SidebarTip as Sidebar } from '../../components/Sidebar/sidebar';
 import { useNavigate } from 'react-router-dom';
 import styles from './Home.module.css';
-import getUser from '../../utils/getUser'; // Ajuste o caminho conforme necessário
+import Username from '../../components/getUsername';
 
 const Home = () => {
     const [items, setItems] = useState([]);
@@ -67,7 +67,7 @@ const Home = () => {
                 <Box pad="medium" background="light-2" fill>
                     <Heading level="2" margin="0px 0px 0px 4px" color="#3c6aaf">
                         Dados
-                        <getUser />
+                        <Username />
                     </Heading>
                     <Card background="white" margin={{ top: 'small' }} elevation="small" round="small" fill>
                         <CardHeader pad="medium" background="#3c6aaf">
